dce134bc2ae9408b4726ea4a432d9e6e5004a651
Eyob <eyobgeberkidan@gmail.com>
2025-12-23 02:22:08 +0000

    Add MariaDB authentication using SQLAlchemy and Flask Login
    - Integrated SQLAlchemy ORM with a local MariaDB database using
      environment-based configuration.
    - Implemented secure user authentication with password hashing, session management via
      Flask-Login, and protected routes.
    - Verified database connectivity,table creation, and successful login flow.

--- server/app.py
+++ server/app.py
@@ -1,6 +1,14 @@
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
+from flask_login import (
+    LoginManager,
+    login_user,
+    logout_user,
+    login_required,
+    current_user,
+)
+from models import db, User
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.callbacks import SubscribeCallback
@@ -10,11 +18,24 @@ load_dotenv()
 
 PUBLISH_KEY = os.getenv("PUBNUB_PUBLISH_KEY")
 SUBSCRIBE_KEY = os.getenv("PUBNUB_SUBSCRIBE_KEY")
-
 if not PUBLISH_KEY or not SUBSCRIBE_KEY:
     raise ValueError("PubNub keys are not set in environment variables.")
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", "dev-secret-key")
+
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DB = os.getenv("MYSQL_DB")
+
+DATABASE_URI = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+print(DATABASE_URI)  # debug
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
+
 
 latest_data = {}
 
@@ -54,6 +75,40 @@ def light_on():
 def light_off():
     pubnub.publish().channel("home-automation-control").message("TURN_LIGHT_OFF").sync()
     return redirect("/")
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(email=email).first()
+
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for("dashboard"))
+
+        return "Invalid login", 401
+
+    return render_template("login.html")
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
